54bbe3f81887a621952485f65da45bb2aa1ab704
Sunny Pandey <121501430+Seeker002@users.noreply.github.com>
2024-04-05 08:12:05 +0530

    Update Readme.m

--- Adafruit Motor Shield V1/Readme.m
+++ Adafruit Motor Shield V1/Readme.m
@@ -1,1 +1,3 @@
-
+Keep the files in the same directory
+The Simulink file for adafruit shield V1 has been built for Matlab version R2023b
+To run the motor driver for your connection, open the " Stepper Motor" block, Click on "Open editor" there and you see the S-function menu, on top, hit "Build" button to run for your Simulink version.
